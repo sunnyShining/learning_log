0dd0ce3d0dccb5ecbde288fe36136c022f21298a
sunshine <17765293970@163.com>
2018-05-29 22:42:39 +0800

    Learn

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,7 +2,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'learning_logs'
 urlpatterns = [
 	# 主页
 	path('', views.index, name = 'index'),
+	# 显示所有的主题
+	path('topics/', views.topics, name='topics'),
+	path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
 ]
